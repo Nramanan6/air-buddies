3b5314419f8e4f38dd81ef4aae7c5d59dfbaf4f0
Nikhil Ramanan <nramanan6@gatech.edu>
2016-09-25 02:22:46 -0400

    DELTA API DEFEATED

--- AirBuddies/app/src/main/java/com/airbud/api/RetrofitCall.java
+++ AirBuddies/app/src/main/java/com/airbud/api/RetrofitCall.java
@@ -1,10 +1,12 @@
 package com.airbud.api;
 
+import com.airbud.entity.City;
 import com.airbud.entity.Link;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -15,10 +17,10 @@ import retrofit2.http.Query;
 public interface RetrofitCall {
 
     @GET("status")
-    Call<Link> getLinkInfo(
+    Call<ParseDeltaJSON> getLinkInfo(
             @Query("flightNumber") String flightNumber,
-            @Query("flightOriginDate") String flightDate);
-
+            @Query("flightOriginDate") String flightDate,
+            @Query("apikey") String apikey);
     public static final String BASE_URL = "https://demo30-test.apigee.net/v1/hack/";
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
